4f54686c10d5da9d0fe782ae0b34e79459cc13c6
Al Zahraa <149786518+Z00Z1@users.noreply.github.com>
2026-02-23 17:01:34 +0000

    Update hardhat.config.cjs

--- blockchain/hardhat.config.cjs
+++ blockchain/hardhat.config.cjs
@@ -5,7 +5,7 @@ module.exports = {
   networks: {
     ganache: {
       url: "http://127.0.0.1:8545",
-      accounts: ["0xc1728a8efeea1684d477087aa1d29cc52e033bb3a2bbbf690652a210193e00ed"]
+      accounts: ["0x0ed67e32242da2781a5647f9bc77cf110109a36e9cc17651ab62559357bef134"]
     }
   }
 };
