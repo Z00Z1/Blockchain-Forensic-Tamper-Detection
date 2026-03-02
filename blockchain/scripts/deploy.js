599cdf875d465e4a4501ce6de549d599b350a508
Al Zahraa <149786518+Z00Z1@users.noreply.github.com>
2026-03-02 08:40:01 +0000

    Update deploy.js

--- blockchain/scripts/deploy.js
+++ blockchain/scripts/deploy.js
@@ -1,12 +1,15 @@
 async function main() {
   const EvidenceRegistry = await ethers.getContractFactory("EvidenceRegistry");
-  const contract = await EvidenceRegistry.deploy();
-  await contract.deployed();
+  const evidence = await EvidenceRegistry.deploy();
 
-  console.log("Contract deployed to:", contract.address);
+  await evidence.deployed();
+
+  console.log("EvidenceRegistry deployed to:", evidence.address);
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
